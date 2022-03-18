f897ad2ca7f38dc9d8ded6145611f2afb7eb38a6
sjiang29 <69825128+sjiang29@users.noreply.github.com>
2022-03-18 16:51:07 -0700

    push model

--- src/test/java/AutoTests.java
+++ src/test/java/AutoTests.java
@@ -2,8 +2,13 @@ import com.google.protobuf.ByteString;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import proto.MsgInfo;
+import utils.Config;
 
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+
+import static framework.Broker.logger;
+import static utils.Utility.getCheckSum;
 
 public class AutoTests {
 
@@ -32,5 +37,35 @@ public class AutoTests {
         // String.valueOf doesn't work properly here
         String sentContent = String.valueOf(sentMsg.getContent());
         Assertions.assertNotEquals(msg, sentContent);
+    }
+
+    @Test
+    public void testWriteToFile(){
+        String sentFilePath = "sent.txt";
+        String receivedFilePath = "received.txt";
+        PrintWriter pw =  null;
+
+        try (FileInputStream fileInputStream = new FileInputStream(sentFilePath);
+             BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream, StandardCharsets.ISO_8859_1))) {
+            String line;
+
+            FileWriter fileWriter = new FileWriter(receivedFilePath);
+            pw = new PrintWriter(fileWriter);
+
+            while ((line = br.readLine()) != null) {
+                pw.println(line);
+            }
+            pw.flush();
+
+        }catch (FileNotFoundException e) {
+            System.out.println("File does not exist!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(pw != null){
+                pw.close();
+            }
+            Assertions.assertEquals(getCheckSum(sentFilePath), getCheckSum(receivedFilePath));
+        }
     }
 }
